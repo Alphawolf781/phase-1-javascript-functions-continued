1f4d668bd0d71f88d2d4c980a127bb6e77534c99
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-06-26 20:58:11 +0300

    Add to complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+ describe("index.js", () => {
+});
+
+function saturdayFun (end="roller-skate"){
+    return `This Saturday, I want to ${end}!`
+}
+   function overridensaturdayFun(newend="bathe my dog"){
+    return saturdayFun(newend);
+   }
+overridensaturdayFun();
+
+function mondayWork(phrase="go to the office"){
+    return `This Monday, I will ${phrase}.`
+}
+mondayWork()
+
+function wrapAdjective(symbol='*'){
+   return function(value="a dedicated programmer"){
+ return `You are ${symbol}${value}${symbol}!`
+   }
+}
